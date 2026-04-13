8e0f1f441b000522c17ea1bbf5bd898ff4d9ed4a
neha <nehapunnath@gmail.com>
2026-04-13 18:58:22 +0530

    'first'

--- src/service/base_urls.js
+++ src/service/base_urls.js
@@ -1,6 +1,9 @@
 // const base_urls="http://localhost:5000"
 
-const base_urls="https://raynott-edupot-backend.onrender.com"
+// const base_urls="https://raynott-edupot-backend.onrender.com"
+
+const base_urls="https://raynott-edupot.onrender.com"
+
 
 
 export default base_urls
